e9b636947386ddf6d40864893beb6f7282c8f217
Tim Moreton <tmoreton89@gmail.com>
2024-01-12 15:56:21 -0300

    add ga tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,20 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import Script from 'next/script'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=Y5M58FNNSX" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'Y5M58FNNSX');
+        `}
+      </Script>
       <Header />
       <main className="flex-auto">{children}</main>
       <Footer />
